644c16399fbeed996a586b1e5d7a665be4d3706d
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-08-24 21:33:57 +0700

    feat: add Entity for a project

---
+++ src/main/java/com/hd/jwelery/entity/Collection.java
@@ -0,0 +1,35 @@
+package com.hd.jwelery.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "collections")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Collection {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long collectionId;
+
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    private String imageUrl;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "collection")
+    private List<Product> products;
+}
