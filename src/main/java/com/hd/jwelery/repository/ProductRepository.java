2a94146ab96d37d950bd8096100bdb8648944a19
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-08-25 22:30:38 +0700

    feat: add ProductRepository with query methods

---
+++ src/main/java/com/hd/jwelery/repository/ProductRepository.java
@@ -0,0 +1,18 @@
+package com.hd.jwelery.repository;
+
+import com.hd.jwelery.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllBy();
+    Product findByName(String productName);
+    List<Product> findAllByCategory_CategoryName(String categoryName);
+    List<Product> findAllByCollection_Name(String collectionName);
+    List<Product> findAllByCategory_CategoryNameAndCollection_Name(String categoryName, String collectionName);
+    List<Product> findAllByPriceBetween(Long minPrice, Long maxPrice);
+    List<Product> findAllByStockQuantityGreaterThan(int stock);
+    List<Product> findAllByMaterial(String material);
+}
+
