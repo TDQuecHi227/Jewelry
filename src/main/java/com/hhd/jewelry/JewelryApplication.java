aa5bb4aed17d5d749b593b7088b12bc26279bcf2
TQQuecHi227 <huyt02731@gmail.com>
2025-08-26 11:13:26 +0700

    rename: JewelryApplication

--- src/main/java/com/hhd/jewelry/JewelryApplication.java
+++ src/main/java/com/hhd/jewelry/JewelryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JweleryApplication {
+public class JewelryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JweleryApplication.class, args);
+        SpringApplication.run(JewelryApplication.class, args);
     }
 
 }
