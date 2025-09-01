220c75e32de3b4b1fbca5bc9fd5a16b62cb0c15f
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-09-01 19:57:46 +0700

    refactor: restructure entities with updated fields and relationships

---
+++ src/main/java/com/hhd/jewelry/entity/CartItem.java
@@ -0,0 +1,34 @@
+package com.hhd.jewelry.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cart_items")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column
+    private String size;
+
+    @Column(nullable = false)
+    private Integer price;
+}
