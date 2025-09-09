741385a6a1645506908568f7db0093eaff755ad5
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-09-09 17:00:36 +0700

    feat: enhance CategoryService with listing and conditional save
    - Added `getAllCategories` method to retrieve a list of all categories.
    - Updated `save` method to handle conditional updates based on the existence of a category.

--- src/main/java/com/hhd/jewelry/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/hhd/jewelry/service/impl/CategoryServiceImpl.java
@@ -5,6 +5,9 @@ import com.hhd.jewelry.repository.CategoryRepository;
 import com.hhd.jewelry.service.CategoryService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
@@ -15,13 +18,28 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public Category getCategoryByName(String categoryName) {
-        return categoryRepository.findByCategoryName(categoryName);
+        return categoryRepository.findByName(categoryName).orElse(null);
     }
 
     @Override
-    public Category save(Category category) {
-        return categoryRepository.save(category);
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAllBy();
     }
+
+    @Override
+    public void save(Category category) {
+        Optional<Category> existingCategory = categoryRepository.findByName(category.getName());
+
+        if (existingCategory.isPresent()) {
+            Category existing = existingCategory.get();
+            existing.setImageUrl(category.getImageUrl());
+            categoryRepository.save(existing);
+        }
+        else{
+            categoryRepository.save(category);
+        }
+    }
+
 
     @Override
     public void delete(Category category) {
