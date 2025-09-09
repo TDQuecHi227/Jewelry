f096d9737f158595d80568422d5da684f00c6912
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-09-09 17:02:07 +0700

    feat: update CategoryRepository with enhanced query methods
    - Renamed `findByCategoryName` to `findByName` for consistent naming.
    - Added `findAllBy` to retrieve all categories as a list.
    - Updated method return types for better flexibility (`Optional`, `List`).

--- src/main/java/com/hhd/jewelry/service/ProductService.java
+++ src/main/java/com/hhd/jewelry/service/ProductService.java
@@ -7,12 +7,15 @@ import java.util.List;
 public interface ProductService {
     List<Product> getAllProducts();
     Product getProductByName(String productName);
+    Product getProductBySerialNumber(String serialNumber);
     List<Product> getProductsByCategoryName(String categoryName);
     List<Product> getProductsByCollectionName(String collectionName);
     List<Product> getProductsByCategoryNameAndCollectionName(String categoryName, String collectionName);
-    List<Product> getProductsByPriceBetween(Long minPrice, Long maxPrice);
+    List<Product> getProductsByPriceBetween(Integer minPrice, Integer maxPrice);
     List<Product> getProductsByStockQuantityGreaterThan(int stock);
     List<Product> getProductsByMaterial(String material);
-    Product save(Product product);
+    void save(Product product);
     void delete(Product product);
+    void deleteAll();
+    void resetAutoIncrement();
 }
--- src/main/java/com/hhd/jewelry/service/impl/ProductServiceImpl.java
+++ src/main/java/com/hhd/jewelry/service/impl/ProductServiceImpl.java
@@ -6,6 +6,7 @@ import com.hhd.jewelry.service.ProductService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -15,7 +16,6 @@ public class ProductServiceImpl implements ProductService {
         this.productRepository = productRepository;
     }
 
-
     @Override
     public List<Product> getAllProducts() {
         return productRepository.findAllBy();
@@ -23,12 +23,17 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Product getProductByName(String productName) {
-        return productRepository.findByName(productName);
+        return productRepository.findByName(productName).orElse(null);
+    }
+
+    @Override
+    public Product getProductBySerialNumber(String serialNumber) {
+        return productRepository.findBySerialNumber(serialNumber).orElse(null);
     }
 
     @Override
     public List<Product> getProductsByCategoryName(String categoryName) {
-        return productRepository.findAllByCategory_CategoryName(categoryName);
+        return productRepository.findAllByCategory_Name(categoryName);
     }
 
     @Override
@@ -38,11 +43,11 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public List<Product> getProductsByCategoryNameAndCollectionName(String categoryName, String collectionName) {
-        return productRepository.findAllByCategory_CategoryNameAndCollection_Name(categoryName, collectionName);
+        return productRepository.findAllByCategory_NameAndCollection_Name(categoryName, collectionName);
     }
 
     @Override
-    public List<Product> getProductsByPriceBetween(Long minPrice, Long maxPrice) {
+    public List<Product> getProductsByPriceBetween(Integer minPrice, Integer maxPrice) {
         return productRepository.findAllByPriceBetween(minPrice, maxPrice);
     }
 
@@ -57,12 +62,44 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public Product save(Product product) {
-        return productRepository.save(product);
+    public void save(Product product) {
+        Optional<Product> existingProduct = productRepository.findBySerialNumber(product.getSerialNumber());
+
+        if (existingProduct.isPresent()) {
+            Product existing = existingProduct.get();
+
+            existing.setName(product.getName());
+            existing.setGemstone(product.getGemstone());
+            existing.setMaterial(product.getMaterial());
+            existing.setBrand(product.getBrand());
+            existing.setPrice(product.getPrice());
+            existing.setDiscount(product.getDiscount());
+            existing.setOrder(product.getOrder());
+            existing.setGender(product.getGender());
+            existing.setCategory(product.getCategory());
+            existing.setCollection(product.getCollection());
+            existing.setStockQuantity(product.getStockQuantity());
+            existing.setImageUrls(product.getImageUrls());
+
+            productRepository.save(existing);
+        }
+        else {
+            productRepository.save(product);
+        }
     }
 
     @Override
     public void delete(Product product) {
         productRepository.delete(product);
+    }
+
+    @Override
+    public void deleteAll() {
+        productRepository.deleteAll();
+    }
+
+    @Override
+    public void resetAutoIncrement() {
+        productRepository.resetAutoIncrement();
     }
 }
