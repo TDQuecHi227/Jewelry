801b5e11a23ccde01719e4fdd5d32c16fe9956c5
TQQuecHi227 <huyt02731@gmail.com>
2025-10-04 17:28:29 +0700

    update feat checkout

--- src/main/java/com/hhd/jewelry/repository/CartItemRepository.java
+++ src/main/java/com/hhd/jewelry/repository/CartItemRepository.java
@@ -6,10 +6,13 @@ import com.hhd.jewelry.entity.Product;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CartItemRepository extends JpaRepository<CartItem,Integer> {
     Optional<CartItem> findCartItemByCartAndProduct(Cart cart, Product product);
+
+    List<CartItem> findAllByCart_CartId(Integer cartCartId);
     @Transactional
     void deleteByCart_CartIdAndProduct_Id(Integer cart_id,  Integer product_id);
 }
