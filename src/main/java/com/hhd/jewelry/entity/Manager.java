1fe9fe050360cd1a0ff41373cc86a9cdc29df46e
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-10-06 12:57:42 +0700

    feat: update project

--- src/main/java/com/hhd/jewelry/entity/Manager.java
+++ src/main/java/com/hhd/jewelry/entity/Manager.java
@@ -13,6 +13,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Manager {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
--- src/main/java/com/hhd/jewelry/entity/ManagerBudget.java
+++ src/main/java/com/hhd/jewelry/entity/ManagerBudget.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ManagerBudget {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
